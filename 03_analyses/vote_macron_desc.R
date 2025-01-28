9761f1a6ba89488f575f58ffd1942e83a178aa1b
arsn-dgn <arsenedegnieau@gmail.com>
2025-01-28 15:19:53 +0100

    Clean 2 3 trucs

--- 03_analyses/vote_macron_desc.R
+++ 03_analyses/vote_macron_desc.R
@@ -1,53 +1,61 @@
 ### Premières analyses.
 
+## On merge Pres et le recensement.
+
+match <- merge(Pres, communes, by.x = "DepCom", by.y = "codgeo", all.x = FALSE)
+
 ## On crée une variable pour les voix Macron en pourcentage des votes exprimés.
 
 # Pour 2017
 
-Pres <- Pres %>%
+match <- match %>%
   mutate(pourc_macron_17 = Nb_17_Voix_Macron * 100 / Nb_17_Exprimés)
 
 # Pour 2022
 
-Pres <- Pres %>%
+match <- match %>%
   mutate(pourc_macron_22 = Nb_22_Voix_Macron * 100 / Nb_22_Exprimés)
 
 ## On crée une variable qui calcule la différence de pourcentage entre les deux.
 
-Pres <- Pres %>% 
+match <- match %>% 
   mutate(diff_pourc_macron = pourc_macron_22 - pourc_macron_17)
 
-summary(Pres$diff_pourc_macron)
+# Moyenne, médiane, quartiles de l'ensemble.
 
-names(Pres)
-names(communes)
+summary(match$diff_pourc_macron)
 
-match <- Pres |> 
-  right_join(communes, 
-             by = c("Libellé_commune" = "libgeo"))
-
-match <- match %>%
-  filter(!is.na(Libellé_région))
-
-match <- match %>%
-  filter(!Libellé_région %in% c("Guyane", "La Réunion", "Martinique", "Guadeloupe"))
-
-boxplot(match$diff_pourc_macron)
+# Moyenne, médiane, quintiles de communes rurales (dens = 7, 6 ,5)
 
 match %>%
-  filter(dens %in% c(6, 5, 4)) %>%
-  ggplot(aes(y = diff_pourc_macron)) +
-  geom_boxplot() +
-  labs(title = "Boxplot de diff_pourc_macron", y = "Différence pourcentage Macron")
-
-match %>%
-  filter(dens %in% c(6, 5, 4)) %>%
+  filter(dens %in% c(7, 6, 5)) %>%
   summarise(
     moyenne = mean(diff_pourc_macron, na.rm = TRUE),
     mediane = median(diff_pourc_macron, na.rm = TRUE),
     quartiles = list(quantile(diff_pourc_macron, na.rm = TRUE))
   )
 
-unique(match$Libellé_région)
+# Boxplot
+
+match %>%
+  filter(dens %in% c(7, 6, 5)) %>%
+  ggplot(aes(y = diff_pourc_macron)) +
+  geom_boxplot()
+
+# Histogramme.
+
+match %>%
+  filter(dens %in% c(7, 6, 5)) %>%
+  ggplot(aes(x = diff_pourc_macron)) +
+  geom_histogram(binwidth = 0.5, fill = "blue", color = "black")
 
 
+
+
+ggplot(match, aes)
+
+unique(match$Libellé_département)
+
+tabel
+
+Pres$DepCom
