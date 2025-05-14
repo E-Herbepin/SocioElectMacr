3cc0b9c83e31897b47096a2598ba141499eef63d
E.Herbepin <emmanuel.herbepin@lilo.org>
2025-05-14 23:13:58 +0200

    l

--- 03_analyses/vote_macron_desc.R
+++ 03_analyses/vote_macron_desc.R
@@ -2,37 +2,9 @@
 
 source("02_scripts/00_setup.R")
 source("02_scripts/01_import.R")
+source("02_scripts/05_recode_PresF.R")
 ## On crée une variable pour les voix Macron en pourcentage des votes exprimés.
 
-PresF <- PresF %>%
-  mutate(pourc_macron_17 = Nb_17_Voix_Macron * 100 / Nb_17_Exprimés)
-
-PresF <- PresF %>%
-  mutate(
-    ## On crée une variable pour les voix Macron en pourcentage des votes exprimés.
-    # Pour 2017
-    pourc_macron_17 = Nb_17_Voix_Macron * 100 / Nb_17_Exprimés,
-    # Pour 2022
-    pourc_macron_22 = Nb_22_Voix_Macron * 100 / Nb_22_Exprimés,
-    ## On crée une variable qui calcule la différence de pourcentage entre les deux.
-    diff_pourc_macron = pourc_macron_22 - pourc_macron_17,
-    
-    cs_pi_pourc = cs_pi_n * 100 / cs_tot,
-    cs_agri_pourc = cs_agri_n * 100 / cs_tot,
-    cs_acce_pourc = cs_acce_n * 100 / cs_tot,
-    
-    dens = as.factor(PresF$dens)
-  )
-
-PresF <- PresF %>%
-  mutate(pourc_macron_22 = Nb_22_Voix_Macron * 100 / Nb_22_Exprimés)
-# summary(PresF$diff_pourc_macron)
-# mean(PresF$diff_pourc_macron, na.rm = TRUE)
-#
-# barplot(PresF$diff_pourc_macron)
-
-PresF <- PresF %>% 
-  mutate(diff_pourc_macron = pourc_macron_22 - pourc_macron_17)
 
 summary(PresF$diff_pourc_macron)
 mean(PresF$diff_pourc_macron, na.rm = TRUE)
